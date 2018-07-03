61fc1bf8c3337b684d3b2084b850e69cbaf07f24
davidkoojohn <davidkoojohn@gmail.com>
2018-07-03 18:35:18 +0800

    try if

---
+++ base/if-random.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+"""
+掷骰子决定做什么事情
+"""
+
+# from random import randint
+
+import random
+
+face = random.randint(1, 6)
+
+print(face)
+
+"""
+if face == 1:
+  print('1')
+elif face == 2:
+  print('2')
+elif face == 3:
+  print('3')
+elif face == 4:
+  print('4')
+elif face == 5:
+  print('5')
+elif face == 6:
+  print('6')
+"""
