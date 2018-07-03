94a92c244158777e7180d8e788f0281e15bedf2c
davidkoojohn <davidkoojohn@gmail.com>
2018-07-03 14:51:28 +0800

    try opt

---
+++ src/input.py
@@ -0,0 +1,17 @@
+
+a = int(input('a='))
+b = int(input('b='))
+
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+
+print('-----//')
+print('%d // %d = %d' % (a, b, a // b))
+
+print('-----%')
+print('%d %% %d = %d' % (a, b, a % b))
+
+print('-----**')
+print('%d ** %d = %d' % (a, b, a ** b))
---
+++ src/var.py
@@ -0,0 +1,18 @@
+
+a = 10
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print('-----//')
+print(a // b)
+
+print('-----%')
+print(a % b)
+
+print('-----**')
+print(a ** b)
+
