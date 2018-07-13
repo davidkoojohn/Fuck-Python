65389cb59f55415a46456bbf4d79bccebbdf2a3c
davidkoojohn <davidkoojohn@gmail.com>
2018-07-13 17:25:46 +0800

    update wirte

--- file/wirte-json.py
+++ file/wirte-json.py
@@ -1,5 +1,13 @@
 import json
 
+"""
+json模块主要有四个比较重要的函数，分别是：
+dump - 将Python对象按照JSON格式序列化到文件中
+dumps - 将Python对象处理成JSON格式的字符串
+load - 将文件中的JSON数据反序列化成对象
+loads - 将字符串的内容反序列化成Python对象
+"""
+
 def main():
 
   mydict = {
