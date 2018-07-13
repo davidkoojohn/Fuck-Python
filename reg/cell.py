db8a547eff0c48954dfa03d2ca9ccdd56fc555d4
davidkoojohn <davidkoojohn@gmail.com>
2018-07-13 18:03:20 +0800

    try reg exm

---
+++ reg/cell.py
@@ -0,0 +1,31 @@
+import re
+
+
+
+def main():
+
+  pattern = re.compile(r'(?<=\D)1[345678]\d{9}(?=\D)')
+  sentence = '''重要的事情说8130123456789遍，我的手机号是13512346789这个靓号，
+    不是15600998765，也是110或119，王大锤的手机号才是15600998765。'''
+
+
+  mylist = re.findall(pattern, sentence)
+  print(mylist)
+  print('--------------------')
+
+  for temp in pattern.finditer(sentence):
+    print(temp.group())
+
+  print('--------------------')
+
+  m = pattern.search(sentence)
+
+  while m:
+    print(m.group())
+    m = pattern.search(sentence, m.end())
+
+
+  pass
+
+if __name__ == '__main__':
+  main()
---
+++ reg/split.py
@@ -0,0 +1,16 @@
+import re
+
+def main():
+  poem = '窗前明月光，疑是地上霜。举头望明月，低头思故乡。'
+  # sentence_list = re.split(r'[，。, .]', poem)
+  sentence_list = re.split(r'[\' \']', poem)
+
+  print(sentence_list)
+  while '' in sentence_list:
+    sentence_list.remove('')
+  print(sentence_list)
+
+  pass
+
+if __name__ == '__main__':
+  main()
