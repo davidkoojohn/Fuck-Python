a657132f097f601338e0299e4947a70c62e07758
davidkoojohn <davidkoojohn@gmail.com>
2018-08-10 18:21:20 +0800

    try CURD

--- alchemy/db.py
+++ alchemy/db.py
@@ -77,42 +77,40 @@ class Tag(Base):
     def __repr__(self):
         return '%s(%r)' % (self.__class__.__name__, self.name)
 
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    faker = Factory.create()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
+    faker_users = [User(
+        username=faker.name(),
+        password=faker.word(),
+        email=faker.email(),
+    ) for _ in range(10)]
 
-Base.metadata.create_all(engine)
+    session.add_all(faker_users)
 
-faker = Factory.create()
-Session = sessionmaker(bind=engine)
-session = Session()
+    faker_categories = [Category(name=faker.word()) for _ in range(10)]
+    session.add_all(faker_categories)
 
-faker_users = [User(
-    username=faker.name(),
-    password=faker.word(),
-    email=faker.email(),
-) for _ in range(10)]
+    faker_tags = [Tag(name=faker.word()) for _ in range(20)]
+    session.add_all(faker_tags)
 
-session.add_all(faker_users)
+    for _ in range(100):
+        article = Article(
+            title=faker.sentence(),
+            content=' '.join(faker.sentences(nb=random.randint(10, 20))),
+            author=random.choice(faker_users),
+            category=random.choice(faker_categories)
+        )
 
-faker_categories = [Category(name=faker.word()) for _ in range(10)]
-session.add_all(faker_categories)
+        for tag in random.sample(faker_tags, random.randint(2, 5)):
+            article.tags.append(tag)
+    
+        session.add(article)
 
-faker_tags = [Tag(name=faker.word()) for _ in range(20)]
-session.add_all(faker_tags)
+    session.commit()
 
-for _ in range(100):
-    article = Article(
-        title=faker.sentence(),
-        content=' '.join(faker.sentences(nb=random.randint(10, 20))),
-        author=random.choice(faker_users),
-        category=random.choice(faker_categories)
-    )
-
-    for tag in random.sample(faker_tags, random.randint(2, 5)):
-        article.tags.append(tag)
-
-    session.add(article)
-
-session.commit()
-
-
-
+    current_user = session.query(User).get(17)
+    print(current_user)
